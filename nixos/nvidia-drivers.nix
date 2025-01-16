60ab670496a901b89ad609ae6d90bda59bb03860
driversline <n526258360@gmail.com>
2025-01-17 00:28:09 +0300

    nvidia-drivers.nix

--- nixos/nvidia-drivers.nix
+++ nixos/nvidia-drivers.nix
@@ -2,4 +2,6 @@ services.xserver.videoDrivers = [ "nvidia" ];
 
 hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
 
-nixpkgs.config.allowUnfree = true;
+nixpkgs.config = {
+  allowUnfree = true;
+};
